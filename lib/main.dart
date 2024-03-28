7de334918546bf7936d58f9894e55d8bdc991651
Shavindu-Keshan <jkskeshan@std.appsc.sab.ac.lk>
2024-03-28 19:58:07 +0530

    i added the button

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,35 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: "PrioLearn",
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 50.0, left: 85),
+              child: Text(
+                "Welcome to",
+                style: TextStyle(fontSize: 50),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 94.0),
+              child: Text(
+                "PrioLearn",
+                style: TextStyle(fontSize: 50, color: Colors.blue),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Button"),
+            ),
+          ],
         ),
       ),
     );
