64845caa969e2f7c0bdeec91c1a77ccca75e5cf0
Shavindu-Keshan <jkskeshan@std.appsc.sab.ac.lk>
2024-03-29 17:19:13 +0530

    add the color button

--- lib/main.dart
+++ lib/main.dart
@@ -15,23 +15,46 @@ class MainApp extends StatelessWidget {
         body: Column(
           children: [
             const Padding(
-              padding: EdgeInsets.only(top: 50.0, left: 85),
+              padding: EdgeInsets.only(top: 50.0, left: 35),
               child: Text(
                 "Welcome to",
                 style: TextStyle(fontSize: 50),
               ),
             ),
             const Padding(
-              padding: EdgeInsets.only(left: 94.0),
+              padding: EdgeInsets.only(left: 50.0),
               child: Text(
                 "PrioLearn",
                 style: TextStyle(fontSize: 50, color: Colors.blue),
               ),
             ),
-            const SizedBox(height: 200),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Log In"),
+            const SizedBox(height: 150),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(300, 100),
+                  backgroundColor: Colors.blue,
+                ),
+                child: const Text(
+                  "Log In",
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(300, 100),
+                  backgroundColor: Colors.blue,
+                ),
+                child: const Text(
+                  "Sign In",
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ),
+              ),
             ),
           ],
         ),
